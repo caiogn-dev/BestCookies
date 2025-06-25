ea8e61f72895504f3f3a2beb3cb7b379dc12d86e
Caio Gonçalves Nascimento <caiogn2002@gmail.com>
2025-06-25 19:59:00 +0000

    atualizando ALLOWED_HOSTS

--- cookies/cookies/settings.py
+++ cookies/cookies/settings.py
@@ -31,9 +31,9 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','bestcookies.onrender.com']
 
 
 # Application definition
