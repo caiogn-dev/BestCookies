0af94a249f9eeeea8437203bd84980f6539d5d71
Caio Gonçalves Nascimento <caiogn2002@gmail.com>
2025-06-27 18:23:50 +0000

    alterando configuraçoes restframework

--- cookies/cookies/settings.py
+++ cookies/cookies/settings.py
@@ -157,16 +157,7 @@ REST_FRAMEWORK = {
 }
 
 
-## SIMPLE JWT CONFIGURATION
-from datetime import timedelta
 
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
-    "AUTH_HEADER_TYPES": ("Bearer",),
-}
 
 CSRF_TRUSTED_ORIGINS = ['https://bestcookies.onrender.com', 'https://bestcookies-wtdk.onrender.com']
 
