4dc61452eb80ea813df45dc11eb5c54a8c800ef1
Caio Gonçalves Nascimento <caiogn2002@gmail.com>
2025-06-27 17:57:50 +0000

    atualizando view do carrinho api

--- cookies/api/views.py
+++ cookies/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from .serializers import CustomerSerializer
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 class MeView(APIView):
     permission_classes = [IsAuthenticated]
@@ -43,6 +44,23 @@ class CartItemViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         customer = Customer.objects.get(user=self.request.user)
         serializer.save(customer=customer)
+
+    def create(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        product_id = request.data.get('product')
+        quantity = int(request.data.get('quantity', 1))
+        if not product_id:
+            return Response({'detail': 'Produto não informado.'}, status=status.HTTP_400_BAD_REQUEST)
+        cart_item, created = CartItem.objects.get_or_create(
+            customer=customer,
+            product_id=product_id,
+            defaults={'quantity': quantity}
+        )
+        if not created:
+            cart_item.quantity += quantity
+            cart_item.save()
+        serializer = self.get_serializer(cart_item)
+        return Response(serializer.data, status=status.HTTP_201_CREATED if created else status.HTTP_200_OK)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
